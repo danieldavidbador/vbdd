d3c9bf7bb5b162dd804900b62c99c92817e2ccd1
Daniel David N. Bador <danieldavidbador@yahoo.com>
2026-02-02 21:38:56 +0800

    update pipeline

--- dataset/augment.py
+++ dataset/augment.py
@@ -1,4 +1,5 @@
 import argparse
+import pandas
 import torch
 
 if __name__ == "__main__":
@@ -9,3 +10,26 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    torch.manual_seed(args.seed)
+
+    generator = torch.jit.load("/opt/ml/processing/input/model/generator.pth")
+    generator.eval()
+
+    test_features = pandas.read_csv("/opt/ml/processing/input/data/features.csv")
+    test_targets = pandas.read_csv("/opt/ml/processing/input/data/targets.csv")
+
+    augmented_features_list = []
+    augmented_targets_list = []
+
+    for class_label in test_targets["target"].unique():
+        class_labels_tensor = torch.full((args.num_samples_per_class,), class_label, dtype=torch.long)
+        with torch.no_grad():
+            generated_data = generator(class_labels_tensor).cpu().numpy()
+        augmented_features_list.append(pandas.DataFrame(generated_data, columns=test_features.columns))
+        augmented_targets_list.append(pandas.DataFrame({"target": class_label}, index=range(args.num_samples_per_class)))
+
+    augmented_features = pandas.concat(augmented_features_list, ignore_index=True)
+    augmented_targets = pandas.concat(augmented_targets_list, ignore_index=True)
+
+    augmented_features.to_csv("/opt/ml/processing/output/train/features.csv", index=False)
+    augmented_targets.to_csv("/opt/ml/processing/output/train/targets.csv", index=False)
--- pipeline.py
+++ pipeline.py
@@ -1,37 +1,159 @@
 from sagemaker import get_execution_role
 
-from sagemaker.workflow.steps import ProcessingStep
+from sagemaker.workflow.steps import ProcessingStep, TrainingStep, ModelStep
+from sagemaker.workflow.pipeline import Pipeline
+from sagemaker.workflow.parameters import ParameterString
 from sagemaker.workflow.pipeline_context import PipelineSession
 
-from sagemaker.processing import ProcessingInput, ProcessingOutput
-from sagemaker.sklearn.processing import SKLearnProcessor
+from sagemaker.model import Model
+from sagemaker.training import PyTorch, TrainingInput
+from sagemaker.processing import SKLearnProcessor, ProcessingInput, ProcessingOutput
 
 session = PipelineSession()
 
+dataset_path = ParameterString(
+    name="DatasetS3Path",
+    default_value="s3://my-bucket/my-dataset/data.csv"
+)
+
 sklearn_processor = SKLearnProcessor(
-    framework_version="0.20.0",
+    framework_version="1.2-1",
     role=get_execution_role(),
     instance_type="ml.t3.medium",
     instance_count=1,
     sagemaker_session=session
 )
 
-step_args = sklearn_processor.run(
-    code="split_dataset.py",
-    inputs=[
-        ProcessingInput(source="shared/data.csv", destination="/opt/ml/processing/input")
-    ],
-    outputs=[
-        ProcessingOutput(source="/opt/ml/processing/train"),
-        ProcessingOutput(source="/opt/ml/processing/validation"),
-        ProcessingOutput(source="/opt/ml/processing/test")
-    ]
-)
-
-step_process = ProcessingStep(
+split_dataset = ProcessingStep(
     name="SplitDataset",
-    processor=sklearn_processor,
-    step_args=step_args
+    step_args=sklearn_processor.run(
+        code="dataset/split.py",
+        inputs=[
+            ProcessingInput(
+                source=dataset_path,
+                destination="/opt/ml/processing/input"
+            )
+        ],
+        outputs=[
+            ProcessingOutput(output_name="train", source="/opt/ml/processing/train"),
+            ProcessingOutput(output_name="validation", source="/opt/ml/processing/validation"),
+            ProcessingOutput(output_name="test", source="/opt/ml/processing/test")
+        ]
+    )
 )
 
-# ml.g4dn.xlarge
+gan_estimator = PyTorch(
+    entry_point="gan/train.py",
+    role=get_execution_role(),
+    framework_version="1.12.0",
+    py_version="py38",
+    instance_count=1,
+    instance_type="ml.g4dn.xlarge",
+    hyperparameters={
+        "epochs": 10,
+        "step-size": 5,
+        "batch-size": 64,
+        "learning-rate": 0.01,
+    },
+    sagemaker_session=session
+)
+
+train_gan_model = TrainingStep(
+    name="TrainGanModel",
+    step_args=gan_estimator.fit(
+        inputs={
+            "train": TrainingInput(
+                s3_data=split_dataset.properties.ProcessingOutputConfig.Outputs["train"].S3Output.S3Uri,
+                content_type="text/csv"
+            )
+        }
+    )
+)
+
+gan_model = Model(
+    model_data=train_gan_model.properties.ModelArtifacts.S3ModelArtifacts,
+    role=get_execution_role(),
+    sagemaker_session=session,
+)
+
+create_gan_model = ModelStep(
+    name="CreateGanModel",
+    step_args=gan_model.create(
+        instance_type="ml.m5.large",
+        accelerator_type="ml.eia1.medium"
+    )
+)
+
+augment_dataset = ProcessingStep(
+    name="AugmentDataset",
+    step_args=sklearn_processor.run(
+        code="dataset/augment.py",
+        inputs=[
+            ProcessingInput(
+                source=split_dataset.properties.ProcessingOutputConfig.Outputs["train"].S3Output.S3Uri,
+                destination="/opt/ml/processing/input/data"
+            ),
+            ProcessingInput(
+                source=create_gan_model.properties.ModelArtifacts.S3ModelArtifacts,
+                destination="/opt/ml/processing/input/model"
+            ),
+        ],
+        outputs=[
+            ProcessingOutput(output_name="train", source="/opt/ml/processing/output/train")
+        ]
+    )
+)
+
+detector_estimator = PyTorch(
+    entry_point="detector/train.py",
+    role=get_execution_role(),
+    framework_version="1.12.0",
+    py_version="py38",
+    instance_count=1,
+    instance_type="ml.g4dn.xlarge",
+    hyperparameters={
+        "epochs": 15,
+        "step-size": 5,
+        "batch-size": 64,
+        "learning-rate": 0.01,
+    },
+    sagemaker_session=session
+)
+
+train_detector_model = TrainingStep(
+    name="TrainDetectorModel",
+    step_args=detector_estimator.fit(
+        inputs={
+            "train": TrainingInput(
+                s3_data=augment_dataset.properties.ProcessingOutputConfig.Outputs["train"].S3Output.S3Uri,
+                content_type="text/csv"
+            ),
+            "validation": TrainingInput(
+                s3_data=split_dataset.properties.ProcessingOutputConfig.Outputs["validation"].S3Output.S3Uri,
+                content_type="text/csv"
+            )
+        }
+    )
+)
+
+detector_model = Model(
+    model_data=train_detector_model.properties.ModelArtifacts.S3ModelArtifacts,
+    role=get_execution_role(),
+    sagemaker_session=session,
+)
+
+create_detector_model = ModelStep(
+    name="CreateDetectorModel",
+    step_args=detector_model.create(
+        instance_type="ml.m5.large",
+        accelerator_type="ml.eia1.medium"
+    )
+)
+
+pipeline = Pipeline(
+    name = "VibrationBasedDamageDetectionModelPipeline",
+    parameters = [dataset_path],
+    steps = [split_dataset, train_gan_model, create_gan_model, augment_dataset, train_detector_model, create_detector_model],
+)
+
+pipeline.upsert(role_arn=get_execution_role())
